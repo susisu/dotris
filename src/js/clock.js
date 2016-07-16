0ba796262ea7ecdda79102f2b8783da00b83f16d
Susisu <susisu2413@yahoo.co.jp>
2016-07-17 03:23:58 +0900

    Add clock module

---
+++ src/js/clock.js
@@ -0,0 +1,48 @@
+/*
+ * dotris / clock.js
+ * copyright (c) 2016 Susisu
+ */
+
+"use strict";
+
+import { EventEmitter2 } from "eventemitter2";
+
+const CLOCK_INTERVAL = 16;
+
+export class Clock extends EventEmitter2 {
+    constructor(frequency) {
+        super();
+        this._frequency  = frequency;
+        this._total      = 0.0;
+        this._oldTime    = undefined;
+        this._intervalId = undefined;
+        this._onTick     = () => {
+            let now       = Date.now();
+            let interval  = now - this._oldTime;
+            this._oldTime = now;
+            this._total  += interval / 1000.0;
+            while (this._total >= 1.0 / this._frequency) {
+                this._total -= 1.0 / this._frequency;
+                this.emit("clock", now);
+            }
+        };
+    }
+
+    get frequency() {
+        return this._frequency;
+    }
+
+    set frequency(val) {
+        this._frequency = val;
+        this._total     = 0.0;
+    }
+
+    start() {
+        this._oldTime    = Date.now();
+        this._intervalId = window.setInterval(this._onTick, CLOCK_INTERVAL);
+    }
+
+    stop() {
+        window.clearInterval(this._intervalId);
+    }
+}
