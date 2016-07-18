97bc710018c1ee167f65e4fa4445af452c9cd066
Susisu <susisu2413@yahoo.co.jp>
2016-07-19 00:58:11 +0900

    Move GameManager class to game-manager module etc.

--- src/js/main.js
+++ src/js/main.js
@@ -5,118 +5,16 @@
 
 "use strict";
 
-import { Game } from "./game.js";
+import { TitleManager } from "./title-manager.js";
+import { GameManager } from "./game-manager.js";
 
 window.addEventListener("load", main);
 
 function main() {
     window.removeEventListener("load", main);
-    let gameConfig = {
-        width : 10, // max: Math.floor(window.innerWidth * window.devicePixelRatio),
-        height: 20  // max: Math.floor(window.innerHeight * window.devicePixelRatio)
-    };
-    let gameManager = new GameManager();
-    gameManager.scaling = true;
-    gameManager.start(gameConfig);
-}
 
-const GW_BORDER_THICKNESS = 1;
+    let title = new TitleManager();
+    let game = new GameManager();
 
-class GameManager {
-    constructor() {
-        this._wrapper = window.document.getElementById("game-wrapper");
-
-        this._width  = 0;
-        this._height = 0;
-
-        this._game = null;
-
-        this._scaling      = false;
-        this._hiResolution = false;
-
-        this._center();
-        this._rescale(true);
-
-        window.addEventListener("resize", () => {
-            this._center();
-            this._rescale(false);
-        });
-    }
-
-    get scaling() {
-        return this._scaling;
-    }
-
-    set scaling(val) {
-        this._scaling = val;
-        this._rescale(true);
-    }
-
-    get hiResolution() {
-        return this._hiResolution;
-    }
-
-    set hiResolution(val) {
-        this._hiResolution = val;
-        this._rescale(true);
-    }
-
-    _center() {
-        this._wrapper.style.top  = `${(window.innerHeight - (this._height + GW_BORDER_THICKNESS * 2)) * 0.5}px`;
-        this._wrapper.style.left = `${(window.innerWidth - (this._width + GW_BORDER_THICKNESS * 2)) * 0.5}px`;
-    }
-
-    _rescale(full) {
-        if (this._scaling) {
-            let scale = this._width / this._height >= window.innerWidth / window.innerHeight
-                    ? window.innerWidth / this._width
-                    : window.innerHeight / this._height;
-            this._wrapper.style.transform = `scale(${scale},${scale})`;
-        }
-        else if (full) {
-            if (this._hiResolution) {
-                let scale = 1.0 / window.devicePixelRatio;
-                this._wrapper.style.transform = `scale(${scale},${scale})`;
-            }
-            else {
-                this._wrapper.style.transform = "none";
-            }
-        }
-    }
-
-    start(config) {
-        if (!this._game) {
-            this._width  = config.width;
-            this._height = config.height;
-
-            this._wrapper.style.width  = `${this._width}px`;
-            this._wrapper.style.height = `${this._height}px`;
-            this._center();
-            this._rescale(false);
-
-            this._game = new Game(this._width, this._height, {});
-            this._game.canvas.className  = "game-canvas";
-            this._game.canvas.style.top  = `${-this._game.topOffset}px`;
-            this._game.canvas.style.left = `${-this._game.leftOffset}px`;
-            this._wrapper.appendChild(this._game.canvas);
-        }
-    }
-
-    pause() {
-        if (this._game) {
-            // pause
-        }
-    }
-
-    resume() {
-        if (this._game) {
-            // resume
-        }
-    }
-
-    quit() {
-        if (this._game) {
-            // quit
-        }
-    }
+    title.show();
 }
