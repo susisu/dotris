6ab2e258ab12a6b7db5fe773895e1caea99a3397
Susisu <susisu2413@yahoo.co.jp>
2016-07-19 21:26:00 +0900

    Add main loop

--- src/js/main.js
+++ src/js/main.js
@@ -14,7 +14,31 @@ function main() {
     window.removeEventListener("load", main);
 
     let title = new TitleManager();
-    let game = new GameManager();
+    let game  = new GameManager();
 
+    title.once("play", onGamePlay);
     title.show();
+
+    function onGamePlay(config) {
+        title.hide();
+
+        game.scaling      = config.scaling;
+        game.hiResolution = config.hiResolution;
+        // TODO: game.colorScheme
+
+        game.once("quit", onGameQuit);
+        game.show();
+        game.start({
+            width   : config.width,
+            height  : config.height,
+            autoPlay: config.autoPlay
+        });
+    }
+
+    function onGameQuit() {
+        game.hide();
+
+        title.once("play", onGamePlay);
+        title.show();
+    }
 }
